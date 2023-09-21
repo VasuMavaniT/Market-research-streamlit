1063966e00fc23a1281b1f68166ed611871e4059
Vasu Mavani <vasu.mavani@thirdray.ai>
2023-09-21 10:54:21 +0000

    Adding files for UI

---
+++ Example.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+
+# Streamlit App Title
+st.title("CSV Analysis App")
+
+# File Upload Section
+st.sidebar.header("Upload CSV File")
+uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+
+# Check if a file is uploaded
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+
+    # Check if the CSV file has headers
+    if not df.columns.any():
+        st.warning("Please add headers to the CSV file.")
+    else:
+        # Dependent Variable Selection (Radio Button)
+        st.sidebar.header("Select Dependent Variable (Y)")
+        dependent_variable = st.sidebar.radio("Select one dependent variable", df.columns)
+
+        # Independent Variables Selection (Checkbox)
+        st.sidebar.header("Select Independent Variables (X)")
+        independent_variables = st.sidebar.multiselect("Select one or more independent variables", df.columns)
+
+        # Check for duplicate selections
+        if dependent_variable in independent_variables:
+            st.warning("Please ensure the independent and dependent variables are different.")
+        elif not dependent_variable:  # Check if a dependent variable is selected
+            st.warning("Please select a dependent variable.")
+        elif not independent_variables:  # Check if independent variables are selected
+            st.warning("Please select at least one independent variable.")
+        else:
+            # Display selected variables
+            st.write(f"Dependent Variable (Y): {dependent_variable}")
+            st.write(f"Independent Variables (X): {', '.join(independent_variables)}")
+
+            # User Questions and Responses
+            st.sidebar.header("Ask Questions")
+            user_question = st.sidebar.text_input("Enter your question:")
+            
+            # Check if the user has entered a question
+            if user_question:
+                with open(uploaded_file.name, "wb") as f:
+                    f.write(uploaded_file.read())
+
+                ask = f'''{user_question} \n
+                Dependent Variable (Y): {dependent_variable} \n
+                Independent Variables (X): {independent_variables} \n
+                CSV file: {uploaded_file.name}
+                '''
+                response = "This is a sample response to your question."
+                st.subheader("Response")
+                st.write(response)
+
+                print(ask) 
+                
+                # Hide the data summary
+                st.markdown("---")  # Add a horizontal line to separate sections
+            else:
+                st.warning("Please enter a question to proceed.")
+else:
+    st.warning("Please upload a CSV file and select variables to proceed.")
---
+++ Kernel.py
@@ -0,0 +1,46 @@
+import semantic_kernel as sk
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion, OpenAITextEmbedding
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, AzureTextEmbedding
+from semantic_kernel.connectors.memory.chroma import ChromaMemoryStore
+import os 
+import shutil
+import pandas as pd
+import numpy as np
+
+async def make_kernel(memory_name, collection_name, csv_name):
+    kernel = sk.Kernel()
+
+    endpoint = "https://thirdray-openai-demo-instance-us-east.openai.azure.com"
+    api_key = "917bf6ea50214df7a19a1bf1572aab3d"
+    deployment = 'gpt-4'
+
+    kernel.add_text_completion_service("azureopenaicompletion", AzureChatCompletion(deployment, endpoint, api_key))
+    kernel.add_text_embedding_generation_service("ada", AzureTextEmbedding("text-embedding-ada-002", endpoint, api_key))
+
+    print("Kernel is ready")
+
+    kernel.register_memory_store(memory_store=ChromaMemoryStore(persist_directory=memory_name))
+    # print("Made two new services attached to the kernel and made a Chroma memory store that's persistent.")
+
+    ### ONLY DELETE THE DIRECTORY IF YOU WANT TO CLEAR THE MEMORY
+    ### OTHERWISE, SET delete_dir = True
+
+    delete_dir = False
+
+    if (delete_dir):
+        dir_path = memory_name
+        shutil.rmtree(dir_path)
+        kernel.register_memory_store(memory_store=ChromaMemoryStore(persist_directory=dir_path))
+        print("⚠️ Memory cleared and reset")
+
+    df = pd.read_csv(csv_name)
+
+    df_string = df.to_string(index=False)
+    df_list = df_string.split('\n')
+
+    memoryCollectionName = collection_name
+    for i in range(len(df_list)):
+        await kernel.memory.save_information_async(memoryCollectionName, id=f"df_list-{i}", text=f"{df_list[i]}")
+
+    return kernel
+    
